ad73effc2c4c6595b82d7515a1a28d11e9f0a9dc
GalinovskayaA <galiale@bk.ru>
2021-08-19 21:36:36 +0300

    add emojis

--- src/components/chat/ChatPage.tsx
+++ src/components/chat/ChatPage.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Picker, {IEmojiData} from 'emoji-picker-react';
 import Message from "../dialogs/Messege/Message";
 
 const wsChannel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
@@ -30,18 +31,21 @@ export const MessagesChat: React.FC = () => {
             let newMessages = JSON.parse(e.data)
             setMessages((prevMessages) => [...prevMessages, ...newMessages])
         })
-    }, [messages])
+    }, [])
 
     return <>
-        <div style={{height: '20em', overflow: 'auto'}}>
-            {messages.map((m, index) => <Message key={index} userId={m.userId} message={m.message} userName={m.userName}
-                                        photo={m.photo}/>)}
+        <div style={{height: '20em', overflowY: 'auto'}}>
+            {messages.map((m, index) => <Message key={index} userId={m.userId} message={m.message}
+                                                 userName={m.userName} photo={m.photo}/>)}
         </div>
     </>
 }
 
 export const AddMessageForm: React.FC = () => {
     const [message, setMessage] = useState<string>('')
+    const [chosenEmoji, setChosenEmoji] = useState<IEmojiData>();
+
+
     const sendMessage = () => {
         if (!message) {
             return;
@@ -49,14 +53,25 @@ export const AddMessageForm: React.FC = () => {
         wsChannel.send(message)
         setMessage('')
     }
+    const onEmojiClick = (event: React.MouseEvent, emojiObject: IEmojiData) => {
+        setChosenEmoji(emojiObject);
+        setMessage(message + emojiObject.emoji);
+    };
     return <>
         <div>
             <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}> </textarea>
         </div>
         <div>
-            <button onClick={sendMessage}>Send</button>
+            <button onClick={sendMessage} disabled={wsChannel.readyState !== WebSocket.OPEN}>Send</button>
         </div>
-
+        <div>
+            {chosenEmoji ? (
+                <span>You chose: {chosenEmoji.emoji}</span>
+            ) : (
+                <span>No emoji Chosen</span>
+            )}
+            <Picker onEmojiClick={onEmojiClick} preload={true}/>
+        </div>
     </>
 }
 
